524ef5e96c3821645fdc6f7f1415841420254c67
nat <natg.inf@gmail.com>
2018-07-02 22:22:31 +0200

    Correct noReverseMatch at / from pythonanywhere - attempt 1

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .models import Post
 # TODO: create a 404 page with get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):

675c1c2edbad6219854fce13b00f4a12ba93a501
nat <natg.inf@gmail.com>
2018-07-02 07:33:13 +0200

    Add post detail template and view

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,11 @@ from . import views
 # . import views means every view from the blog application
 
 urlpatterns = [
-    re_path(r'^$', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 # or try re_path(r'^$', views.post_list, name='post_list'),
 # path('', views.post_list, name='post_list'),
+
+#  re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+#path('post/<int:pk>/', views.post_detail, name='post_detail'),//not zorking
